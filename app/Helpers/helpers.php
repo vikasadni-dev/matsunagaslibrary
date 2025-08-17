4c29be59fe44fd0ac34707301d48e044d3a82812
Novika Sari Dwi Cahyani <vikasadni@gmail.com>
2025-08-17 20:20:54 +0700

    Helpers & Composer Dump Autoload

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,32 @@
+<?php
+use App\Models\User;
+
+if (!function_exists("flashMessage")){
+    function flashMessage($message, $type = "success"): void
+    {
+        session()->flash("message", $message);
+        session()->flash("type", $type);
+    }
+}
+
+if (!function_exists("usernameGenerator")){
+    function usernameGenerator(string $name): string
+    {
+        $username = strtolower(preg_replace("/\s+/","-", trim($name)));
+        $original_username = $username;
+        $count = 1;
+
+        while (User::where("username", $username)->exists()) {
+            $username = $original_username . $count;
+            $count++;
+    }
+    return $username;
+}
+}
+
+if (!function_exists("signatureMidtrans")){
+    function signatureMidtrans($order_id, $status_code, $gross_amount, $server_key)
+    {
+        return hash("sha512", $order_id . $status_code . $gross_amount . $server_key);
+    }
+}
